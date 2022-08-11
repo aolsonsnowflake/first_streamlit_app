562f4d67c7901cdf5a61be56dfab161c578ab2d1
aolsonsnowflake <111073808+aolsonsnowflake@users.noreply.github.com>
2022-08-11 09:49:42 -0700

    add pandas, fruit list from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Parents' New Healthy Diner")
 
@@ -10,3 +13,4 @@ streamlit.text('🐿️🦯 Squirrel on a Stick')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
