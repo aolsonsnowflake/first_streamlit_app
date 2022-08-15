b11e34061bd317e2363197ee56773ce2a7c6fd82
aolsonsnowflake <111073808+aolsonsnowflake@users.noreply.github.com>
2022-08-15 13:38:00 -0700

    turn fruityvice data get into function

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# make function for getting fruityvice data
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?', key = "sl_fruit_choice")
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    # put json into pandas dataframe
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
     # display pandas dataframe
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
